98cba15f557816ff5fa7800cec7027dcabe8419e
Enver <mr_epsty@hotmail.com>
2023-09-17 15:11:32 +1000

    looping along

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+let cards = [];
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        cards.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    };
+    return cards;
+};
+
+function countDown (integer) {
+    while (integer >= 0) {
+        console.log(integer--);
+    };
+}
